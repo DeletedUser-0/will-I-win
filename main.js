2a5b1ceb1286b944ff575951d42110ccf722bffe
DeletedUser-0 <71786022+DeletedUser-0@users.noreply.github.com>
2021-08-06 15:14:34 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,141 @@
+class Player {
+    constructor(data) {
+        this.times = data?.times || 0;
+        this.time = data?.time || 0;
+
+        this.money = {
+            total: data?.money?.total || 0,
+            receive: data?.money?.receive || 1,
+            multiplier: data?.money?.multiplier || 1.01,
+            chance: data?.money?.chance || 95,
+            sleep: data?.money?.sleep || 3000,
+        };
+        
+        this.result = data?.result || 0;
+
+        this.upgrade1 = {
+            cost: data?.upgrade1?.cost || 1,
+            level: data?.upgrade1?.level || 0,
+        };
+
+        this.upgrade2 = {
+            cost: data?.upgrade2?.cost || 10,
+            level: data?.upgrade2?.level || 0,
+            effect: data?.upgrade2?.effect || 3000,
+        };
+
+        this.upgrade3 = {
+            bought: data?.upgrade3?.bought || false,
+        };
+
+        this.upgrade4 = {
+            cost: data?.upgrade4?.cost || 60,
+            level: data?.upgrade4?.level || 0,
+        };
+    };
+};
+
+var player = new Player();
+
+var mainGameLoop = window.setInterval(function() {
+    if (OmegaNum.cmp(player.money.sleep, 0) > 0) {
+        player.money.sleep = OmegaNum.sub(player.money.sleep, 7);
+    };
+    if (isNaN(player.money.sleep)) {
+        player.money.sleep = new OmegaNum(player.upgrade2.effect);
+    };
+    if (OmegaNum.cmp(player.money.sleep, 0) < 0) {
+        player.money.sleep = 0;
+    };
+    player.upgrade2.effect = OmegaNum.pow(3000, OmegaNum.pow(0.996, player.upgrade2.level));
+    if (player.upgrade3.bought == true) {
+        add();
+    };
+    if (isNaN(player.upgrade4.cost)) {
+        player.upgrade4.cost = player.upgrade4.cost.array[0];
+    };
+    player.time = OmegaNum.add(player.time, 0.007);
+}, 7);
+
+function add() {
+    if (OmegaNum.cmp(player.money.sleep, 0) == 0) {
+        player.result = Math.random() * 100;
+        player.money.sleep = new OmegaNum(player.upgrade2.effect);
+        player.times += 1;
+        if (OmegaNum.cmp(player.result, player.money.chance) >= 0) {
+            player.money.total = OmegaNum.add(player.money.total, player.money.receive).toFixed(2);
+            player.money.receive = OmegaNum.times(player.money.receive, player.money.multiplier);
+        };
+    };
+};
+
+function upgrade1() {
+    if (OmegaNum.cmp(player.money.total, player.upgrade1.cost) >= 0) {
+        player.money.total = OmegaNum.sub(player.money.total, player.upgrade1.cost).toFixed(2);
+        player.upgrade1.cost = OmegaNum.pow(player.upgrade1.cost, 1.06).times(1.01).toFixed(2);
+        player.money.chance = OmegaNum.div(player.money.chance, OmegaNum.add(OmegaNum.div(0.02, OmegaNum.add(OmegaNum.div(player.upgrade1.level, 15), 1)), 1));
+        player.upgrade1.level = Math.floor(OmegaNum.add(player.upgrade1.level, 1)).toLocaleString("pt-PT");
+    };
+};
+
+function upgrade2() {
+    if (OmegaNum.cmp(player.money.total, player.upgrade2.cost) >= 0) {
+        player.money.total = OmegaNum.sub(player.money.total, player.upgrade2.cost).toFixed(2);
+        player.upgrade2.cost = OmegaNum.pow(player.upgrade2.cost, 1.075);
+        player.upgrade2.level = Math.floor(OmegaNum.add(player.upgrade2.level, 1)).toLocaleString("pt-PT");
+    };
+};
+
+function upgrade3() {
+    if (OmegaNum.cmp(player.money.total, 1000) >= 0) {
+        if (player.upgrade3.bought == false) {
+            player.money.total = player.money.total = OmegaNum.sub(player.money.total, 1000);
+            player.upgrade3.bought = true
+        };
+    };
+};
+
+function upgrade4() {
+    if (OmegaNum.cmp(player.time, player.upgrade4.cost) >= 0) {
+        player.upgrade4.level += 1;
+        player.upgrade4.cost = OmegaNum.mul(player.upgrade4.cost, 1.01).add(60);
+        player.money.receive = OmegaNum.pow(player.money.receive, 1.02).mul(1.01);
+    };
+};
+
+function reset() {
+    player.time = 0;
+    player.money.total = 0;
+    player.money.receive = 1;
+    player.money.multiplier = 1.01;
+    player.money.chance = 95;
+    player.money.sleep = 3000;
+    player.result = 0;
+    player.upgrade1.cost = 1;
+    player.upgrade1.level = 0;
+    player.upgrade2.cost = 10;
+    player.upgrade2.level = 0;
+    player.upgrade2.effect = 3000;
+    player.upgrade3.bought = false;
+    player.upgrade4.cost = 60;
+    player.upgrade4.level = 0;
+    document.getElementById("upgrade3").style.backgroundColor = `white`;
+    player.times = 0;
+    player.time = 0;
+};
+
+function Save() {
+    localStorage.player = JSON.stringify(player);
+};
+
+function Load() {
+    player = new Player(JSON.parse(localStorage.player));
+    console.log("Save loaded");
+    return player.obj || "default";
+};
+
+var mainGameLoop = window.setInterval(function() {
+    Save();
+}, 3 * 1000);
+
+Load();
---
+++ ui.js
@@ -0,0 +1,101 @@
+var mainGameLoop = window.setInterval(function() {
+    if (player.time < 60) {
+        document.getElementById("time").innerHTML = `${Math.floor(player.time)} seconds.`;
+    } else if ((player.time > 60) && (player.time < 3600)) {
+        document.getElementById("time").innerHTML = `${Math.floor(player.time / 60)} minutes and ${Math.floor(player.time % 60)} seconds.`;
+    } else if ((player.time > 3600) && (player.time < 86400)) {
+        document.getElementById("time").innerHTML = `${Math.floor(player.time / 3600)} hours, ${Math.floor((player.time / 60) % 60)}, minutes ${Math.floor(player.time % 60)} and seconds`;
+    } else if ((player.time > 86400) && (player.time < 31536000)) {
+        document.getElementById("time").innerHTML = `${Math.floor(player.time / 86400)} days, ${Math.floor((player.time / 3600) % 24)} hours, ${Math.floor((player.time / 60) % 60)} minutes and ${Math.floor(player.time % 60)} seconds`;
+    } else {
+        document.getElementById("time").innerHTML = `${Math.floor(player.time / 31536000)} years, ${Math.floor(player.time / 86400) % 365} days, ${Math.floor((player.time / 3600) % 24)} hours, ${Math.floor((player.time / 60) % 60)} minutes and ${Math.floor(player.time % 60)} seconds`;
+    };
+
+    document.getElementById("money").innerHTML = `You have ${notate2(player.money.total)}$`;
+
+    document.getElementById("chance").innerHTML = `<p style="font-size: 125%;">Probability: <strong>${player.result.toFixed(2)}%</strong></p> <p style="position: relative; bottom: 15px;">Target: <strong>${notate2(player.money.chance)}%</strong>`;
+    document.getElementById("trymoney").innerHTML = `Try to earn ${notate(player.money.receive)}$.`;
+    document.getElementById("upgrade1").innerHTML = `Decrease target value <br> Cost: ${notate(player.upgrade1.cost)}$ <br> Level: ${player.upgrade1.level}`;
+    if (player.money.sleep > 1000) {
+        document.getElementById("wait").innerHTML = `Wait ${(player.money.sleep/1000).toFixed(2)} seconds to try again <p style="font-size: 75%; position: relative; bottom: 5px;"> Times tried: ${Math.floor(player.times).toLocaleString("pt-PT")}`;
+    } else {
+        document.getElementById("wait").innerHTML = `Wait ${Math.floor(player.money.sleep)}ms to try again <p style="font-size: 75%; position: relative; bottom: 5px;"> Times tried: ${Math.floor(player.times).toLocaleString("pt-PT")}`;
+    };
+    if (player.upgrade2.effect > 1000) {
+        document.getElementById("upgrade2").innerHTML = `Decrease waiting time (${(player.upgrade2.effect/1000).toFixed(2)} seconds) <br> Cost: ${notate(player.upgrade2.cost)}$ <br> Level: ${player.upgrade2.level}`;
+    } else {
+        document.getElementById("upgrade2").innerHTML = `Decrease waiting time (${Math.floor(player.upgrade2.effect)}ms) <br> Cost: ${notate(player.upgrade2.cost)}$ <br> Level: ${player.upgrade2.level}`;
+    };
+    if (player.upgrade3.bought == false) {
+        document.getElementById("upgrade3").innerHTML = `Automatically tries to earn money <br> <br> Cost: ${notate(1000)}$`;
+    } else {
+        document.getElementById("upgrade3").innerHTML = `Automatically tries to earn money <br> <br> Upgraded!`;
+        document.getElementById("upgrade3").style.backgroundColor = `lightgreen`;
+    };
+
+    if ((player.upgrade4.cost >= 60) && (player.upgrade4.cost < 3600)) {
+        document.getElementById("upgrade4").innerHTML = `Increase money receivement <br> Requirement: ${Math.floor(player.upgrade4.cost / 60)} min ${Math.floor(player.upgrade4.cost % 60)} s <br> Level: ${Math.floor(player.upgrade4.level).toLocaleString("pt-PT")}`;
+    } else if ((player.upgrade4.cost >= 3600) && (player.upgrade4.cost < 86400)) {
+        document.getElementById("upgrade4").innerHTML = `Increase money receivement <br> Requirement: ${Math.floor(player.upgrade4.cost / 3600)} hr, ${Math.floor((player.upgrade4.cost / 60) % 60)} min and ${Math.floor(player.upgrade4.cost % 60)} sec <br> Level: ${Math.floor(player.upgrade4.level).toLocaleString("pt-PT")}`;
+    } else if ((player.upgrade4.cost >= 86400) && (player.upgrade4.cost < 31536000)) {
+        document.getElementById("upgrade4").innerHTML = `Increase money receivement <br> Requirement: ${Math.floor(player.upgrade4.cost / 86400)} d ${Math.floor((player.upgrade4.cost / 3600) % 24)} hr ${Math.floor((player.upgrade4.cost / 60) % 60)} min and ${Math.floor(player.upgrade4.cost % 60)} sec <br> Level: ${Math.floor(player.upgrade4.level).toLocaleString("pt-PT")}`;
+    } else {
+        document.getElementById("upgrade4").innerHTML = `Increase money receivement <br> Requirement: ${Math.floor(player.upgrade4.cost / 31536000)} y, ${Math.floor(player.upgrade4.cost / 86400) % 365} d, ${Math.floor((player.upgrade4.cost / 3600) % 24)} hr, ${Math.floor((player.upgrade4.cost / 60) % 60)} min and ${Math.floor(player.upgrade4.cost % 60)} sec <br> Level: ${Math.floor(player.upgrade4.level).toLocaleString("pt-PT")}`;
+    }
+}, 0);
+
+(() => {
+    const tooltip = document.getElementById('tooltip');
+    
+    document.querySelectorAll('[tooltip]').forEach(x => {
+
+        x.addEventListener('mouseenter', function(e) {
+            tooltip.innerHTML = this.getAttribute('tooltip');
+
+            tooltip.style.top = (Math.min(this.offsetTop + this.offsetHeight + 10, window.innerHeight - 10 - tooltip.offsetHeight)) + 'px';
+            tooltip.style.left = (Math.min(Math.max(this.offsetLeft + this.offsetWidth / 2), window.innerWidth - tooltip.offsetWidth - 10)) + 'px';
+
+            tooltip.classList.add('active');
+        });
+
+        x.addEventListener('mouseout', function(e) {
+            tooltip.classList.remove('active');
+        });
+    });
+})();
+
+function notate(n = new OmegaNum(0)) {
+    n = new OmegaNum(n);
+
+    if (n.sign == -1) { return `-${n.abs()}`; }
+    if (isNaN(n.array[0])) { return "NaN"; }
+    if (!isFinite(n.array[0])) { return Infinity; }
+
+    if (!n.array[1]) {
+        let e = Math.floor(Math.log10(n.array[0]));
+        let m = n.array[0] / 10 ** e;
+        return e < 3 ? n.toPrecision(3) : `${Math.floor(n).toLocaleString("pt-BR")}`;
+    } else if (n.array[1] < 2) { 
+        return `${Math.pow(10, n.array[0] - Math.floor(n.array[0])).toPrecision(3)}x10<sup>${Math.floor(n.array[0]).toLocaleString("pt-BR")}</sup>`;
+    } else {
+        return `${"e".repeat(n.array[1])}${Math.floor(n.array[0])}`;
+    };
+};
+
+function notate2(n = new OmegaNum(0)) {
+    n = new OmegaNum(n);
+
+    if (n.sign == -1) { return `-${n.abs()}`; }
+    if (isNaN(n.array[0])) { return "NaN"; }
+    if (!isFinite(n.array[0])) { return Infinity; }
+
+    if (!n.array[1]) {
+        let e = Math.floor(Math.log10(n.array[0]));
+        let m = n.array[0] / 10 ** e;
+        return e < 3 ? n.toFixed(2) : `${Math.floor(n).toLocaleString("pt-BR")}`;
+    } else if (n.array[1] < 2) { 
+        return `${Math.pow(10, n.array[0] - Math.floor(n.array[0])).toPrecision(3)}x10<sup>${Math.floor(n.array[0]).toLocaleString("pt-BR")}</sup>`;
+    } else {
+        return `${"e".repeat(n.array[1])}${Math.floor(n.array[0])}`;
+    };
+};
